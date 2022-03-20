c63605984e5ba5a13ca73183666aa54b97b8b0a0
helocoder <vaidyaabhi3@gmail.com>
2022-03-20 20:35:58 +0530

    Signup and login done

--- Login Sysem/login-valid.php
+++ Login Sysem/login-valid.php
@@ -1,6 +1,26 @@
 <?php
  //connecting
-require 'conn.php';
+require 'Db/conn.php';
 
+$username = "";
+$password = "";
+
+
+if(isset($_POST["loglogin"]))
+{
+     $username = $_POST["logusrname"];
+     $password = $_POST["logpassword"];
+
+   
+     
+
+}
+
+
+
+
+
+
+$sql
 
 ?>
