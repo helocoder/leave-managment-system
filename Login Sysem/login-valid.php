e0744841fd80e2b6c3a253c0d81df7a16fe0d62e
helocoder <vaidyaabhi3@gmail.com>
2022-03-21 08:13:50 +0530

    login signup done

--- Login Sysem/login-valid.php
+++ Login Sysem/login-valid.php
@@ -1,7 +1,7 @@
 <?php
  //connecting
 require 'Db/conn.php';
-
+global $conn;
 $username = "";
 $password = "";
 
@@ -10,8 +10,29 @@ if(isset($_POST["loglogin"]))
 {
      $username = $_POST["logusrname"];
      $password = $_POST["logpassword"];
+     
+     $sql = "select * from user where email = '$username' and password = '$password' " ;
+     $result = mysqli_query($conn,$result);
+     
+     $row = mysqli_nums_rows($result);
+     if($row>0)
+     {
+         $sql_role = "select role from user where email = '$email' and password = '$password'";
 
-      
+         
+         if($sql2 == 0)
+         {
+
+             echo "Hello"..
+
+         }
+        echo "user found "
+
+
+
+
+
+     }
 
 
 }
